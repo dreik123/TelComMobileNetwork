17d19d8c54fc8a4070e80b9c97a3475a3ed97b49
dreik123 <andrey149814@gmail.com>
2021-06-02 13:13:00 +0300

    added TestProject

---
+++ TestProject/main.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+bool isFind(std::string str, std::string command)
+{
+    return str == command;
+}
+
+int main()
+{
+    
+    while(true)
+    {
+        std::vector<std::string> commandArgs;
+        std::string command;
+        
+        std::getline(std::cin, command);
+        std::istringstream iss(command);
+        std::string arg;
+        while(std::getline(iss, arg, ' '))
+        {
+            commandArgs.push_back(arg);
+        }
+        if(isFind("unregister", commandArgs.at(0)))
+        {
+            std::cout << "unregistered" << std::endl;
+        }
+        else if(isFind("register", commandArgs.at(0)))
+        {
+            if(commandArgs.size() > 1)
+            {
+                std::cout << "registered" << std::endl;
+            }
+            else
+            {
+                std::cout << "Invalid command. Must be \"register [arg1]\" " << std::endl;
+            }
+        }
+        else if(isFind("name", commandArgs.at(0)))
+        {
+            if(commandArgs.size() > 1)
+            {
+                std::cout << "named" << std::endl;
+            }
+            else
+            {
+                std::cout << "Invalid command. Must be \"name [arg1]\" " << std::endl;
+            }
+        }
+        else if(isFind("callEnd", commandArgs.at(0)))
+        {
+            std::cout << "end call" << std::endl;
+        }
+        else if(isFind("call", commandArgs.at(0)))
+        {
+            if(commandArgs.size() > 1)
+            {
+                std::cout << "calling" << std::endl;
+            }
+            else
+            {
+                std::cout << "Invalid command. Must be \"call [arg1]\" " << std::endl;
+            }
+        }
+        else if(isFind("answer", commandArgs.at(0)))
+        {
+            std::cout << "answered call" << std::endl;
+        }
+        else if(isFind("reject", commandArgs.at(0)))
+        {
+            std::cout << "rejected call" << std::endl;
+        }
+        else if(isFind("exit", commandArgs.at(0)))
+        {
+            std::cout << "exit programm" << std::endl;
+            break;
+        }
+        else 
+        {
+            std::cout << "invalid operation" << std::endl;
+        }
+    }
+    return 0;
+}
