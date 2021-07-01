356ad6935bbdcc2b9f9a5940b616bb613a7b20bd
dreik123 <andrey149814@gmail.com>
2021-07-01 12:19:17 +0300

    writed meson for test

--- TestProject/include/NetConfAgent.hpp
+++ TestProject/include/NetConfAgent.hpp
@@ -15,52 +15,53 @@ class MobileClient;
  * */
 class NetConfAgent {
 public:
+    virtual ~NetConfAgent();
     /**
      * Initializes private fields of the class
      * */
-    bool initSysrepo();
+    virtual bool initSysrepo();
     /**
      * Gets the requested data from the model
      * \param[in] xpath Path to the requested data
      * \param[out] res The container with the received data. The key is xpath, the value is the value of the attribute
      * */
-    bool fetchData(const std::string& xpath, std::map<std::string, std::string>& res);
+    virtual bool fetchData(const std::string& xpath, std::map<std::string, std::string>& res);
     /**
      * Subscribing for changes in the model and calling the function if they occur
      * \param[in] module The name of the model we are subscribing to changes
      * */
-    bool subscribeForModelChanges(const std::string& module, const std::string& xpath, MobileClient& client);
+    virtual bool subscribeForModelChanges(const std::string& module, const std::string& xpath, MobileClient& client);
     /**
      * Register for providing operational data 
      * \param[in] module The name of the model for providing operational data
      * \param[in] xpath Path to the operational data
      * \param[in] data Container with operational data. The key is xpath, the value is the value of the attribute
      * */
-    bool registerOperData(const std::string& module, const std::string& xpath, const std::map<std::string, std::string>& data, MobileClient& client);
+    virtual bool registerOperData(const std::string& module, const std::string& xpath, const std::map<std::string, std::string>& data, MobileClient& client);
     /**
      * Subscribes to receive rpc from the user and send feedback to the user
      * \param[in] xpath Path to rpc
      * \param[in] output_val_for_rpc Data to send user feedback from rpc
      * */
-    bool subscribeForRpc(const std::string& xpath, const std::map<std::string, std::string>& output_val_for_rpc);
+    virtual bool subscribeForRpc(const std::string& xpath, const std::map<std::string, std::string>& output_val_for_rpc);
     /**
      * Sends a notification to the user
      * \param[in] xpath Path to notification
      * \param[in] values Container for notifications. The key is xpath, the value is the value of the attribute
      * */
-    bool notifySysrepo(const std::string& xpath, const std::map<std::string, std::string>& values);
+    virtual bool notifySysrepo(const std::string& xpath, const std::map<std::string, std::string>& values);
     /**
      * Modifies or creates the requested data
      * \param[in] xpath Path to change data
      * \param[in] value Value to change data
      * */
-    bool changeData(const std::string& xpath, const std::string& value);
+    virtual bool changeData(const std::string& xpath, const std::string& value);
 
     /**
      * Deletes the requested data
      * \param[in] xpath Path to deleting data
      * */
-    bool deleteData(const std::string& xpath);
+    virtual bool deleteData(const std::string& xpath);
 private:
     std::shared_ptr<sysrepo::Session> _sess; /**< sysrepo session */
     std::shared_ptr<sysrepo::Connection> _conn; /**< sysrepo connection */
---
+++ TestProject/test/MobileClientTest.cpp
@@ -0,0 +1,4 @@
+#include "NetConfAgentMock.hpp"
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
